704e6d6b93c9ec19d60f4248b2b0e329cbeb07ba
ZY1YOGI <zy1yogi@gmail.com>
2023-06-14 14:50:08 +0300

    v0.4

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import './globals.css'
 
-export const metadata = {
-  title: 'No Title',
-}
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light" data-theme="light">
-      <body className="antialiased bg-body-light dark:bg-body-dark">{children}</body>
+
+      <body className="antialiased bg-body-light dark:bg-body-dark">
+        {children}
+      </body>
     </html>
   )
 }
