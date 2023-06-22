71eeb157279d65b537757020168db1b6d093995c
ZY1YOGI <zy1yogi@gmail.com>
2023-06-22 16:00:21 +0300

    v1.1.4 add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next"
 import '@/app/globals.css'
-
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   applicationName: 'Future Schools',
@@ -14,6 +14,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
       <body className="antialiased bg-body-light dark:bg-body-dark">
         {children}
+
+
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YLGP0D7K5M"></script>
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YLGP0D7K5M');
+          `}
+        </Script>
+
       </body>
     </html>
   )
--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,10 @@ import Image from 'next/image';
 import { useRef } from 'react';
 import Link from 'next/link';
 import { BiUserCircle } from 'react-icons/bi'
-import { BsBarChartSteps } from 'react-icons/bs'
+import { BsBarChartSteps, BsSearch } from 'react-icons/bs'
 import { MdDarkMode } from 'react-icons/md'
+import { FaBars } from 'react-icons/fa'
+
 
 export default function Header() {
 
@@ -32,43 +34,41 @@ export default function Header() {
     <header className="">
       <nav className="negative">
         {/* left Header */}
-        <div className="flex items-center justify-center space-x-3 cursor-pointer">
-          <Image src="/logo.png" alt="logo future schools" width={50} height={50} priority={true} />
-          <h1 className="text-2xl font-bold dark:text-teal-50">Future</h1>
+        <div className="flex items-center justify-center space-x-1.5 cursor-pointer">
+          <Image src="/logo.png" alt="logo future schools" width={55} height={55} priority={true}/>
+          <Link href='/' className="text-2xl font-bold dark:text-teal-50">Future</Link>
         </div>
         {/* left Header */}
 
 
 
         {/* Negative */}
-        <ul className="nav-links" ref={switchMenu}>
-          <Link className='nav-link' href="/" role="link">Home</Link>
-          <Link className='nav-link' href="/" role="link">Profile</Link>
-          <Link className='nav-link' href="/" role="link">Editor</Link>
-          <Link className='nav-link' href="/" role="link">Services</Link>
+        <ul className='nav-links' ref={switchMenu}>
+          <Link className='nav-link' href="/">Home</Link>
+          <Link className='nav-link' href="/">Profile</Link>
+          <Link className='nav-link' href="/">Editor</Link>
+          <Link className='nav-link' href="/">Services</Link>
           <li className="relative nav-link group" id="nav-link-categories">
-            <button className="flex items-center">Rooms <BsBarChartSteps className='ml-1.5' /> </button>
+            <button className="flex items-center">Rooms <BsBarChartSteps className='ml-1.5' /></button>
             <ul role="menu" className="group-hover:block z-50 text-center absolute hidden w-40 whitespace-nowrap space-y-5 top-8 left-[50%] max-md:left-[12%] border-4 border-x-white border-y-primary rounded-xl py-3 px-1 bg-body-light dark:bg-body-dark">
               <li className=''>
-                <Link className='nav-link min-w-full' href="/" role="link">@</Link>
+                <Link className='nav-link min-w-full' href="/">@</Link>
               </li>
               <li>
-                <Link className='nav-link' href="/" role="link">@</Link>
+                <Link className='nav-link' href="/">@</Link>
               </li>
               <li>
-                <Link className='nav-link' href="/" role="link">@</Link>
+                <Link className='nav-link' href="/">@</Link>
               </li>
               <li>
-                <Link className='nav-link' href="/" role="link">@</Link>
+                <Link className='nav-link' href="/">@</Link>
               </li>
 
             </ul>
           </li>
 
           <button className='btn-search' role="search" id="model-search">
-            <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="26" width="26" xmlns="http://www.w3.org/2000/svg">
-              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
-            </svg>
+            <BsSearch size={25} />
           </button>
 
           <button className='btn-theme' role="switch" id="switch-theme" aria-checked="true" onClick={() => localStorage.getItem('theme') === 'light' ? (document.documentElement.className = 'dark', document.documentElement.setAttribute('data-theme', 'dark'), localStorage.setItem('theme', 'dark')) : (document.documentElement.className = 'light', document.documentElement.setAttribute('data-theme', 'light'), localStorage.setItem('theme', 'light'))}>
@@ -79,32 +79,29 @@ export default function Header() {
 
 
         {/* Right Header */}
-        <button className="hidden max-md:block" onClick={funcSwitchMenu} role="switch">
-          <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" className="dark:text-white" height="30" width="30" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z">
-            </path>
-          </svg>
+        <button className='hidden max-md:block' onClick={funcSwitchMenu} role="switch">
+          <FaBars size={25} className='dark:text-white' />
         </button>
 
-        <Link className='btn-auth' id="link-login" href="/auth">
+        <Link className='btn-auth space-x-3' href="/auth">
           <BiUserCircle size={26} title='login user' />
-          Login</Link>
+          Login
+        </Link>
         {/* Right Header */}
       </nav>
 
-      <section className='container mx-auto flex max-md:flex-col md:items-center mt-12'>
+      <section className='container mx-auto flex max-md:flex-col md:items-center mt-8'>
         <section className="md:w-1/2 max-md:text-center flex-1">
-          <h1 className="text-4xl mb-6 text-[#070439] dark:text-white">Future Private Schools We care about all the details of our students</h1>
-          <p className="font-semibold mb-6 text-[#444444] dark:text-gray-300">
-            Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface
-            without relying on meaningful content.
+          <h1 className="text-3xl mb-5 text-[#070439] dark:text-white max-md:px-1">Future Schools We care about all the details of our students</h1>
+          <p className="font-semibold mb-8 text-[#444444] dark:text-gray-300 max-md:px-2">
+            Modern Future Schools are apprentice schools that offer different departments for students, the Programming Department, the Computer Department, and the Chemical Laboratory Department
           </p>
 
-          <a href="#" className="py-4 px-8 bg-[#372cfa] text-white font-semibold cursor-pointer border-none outline-none transition-all rounded-2xl hover:bg-[#070439]">Get Started</a>
+          <a href="#" className="py-5 px-10 bg-[#372cfa] text-white font-semibold cursor-pointer border-none outline-none transition-all rounded-2xl hover:bg-[#070439]">Get Started</a>
         </section>
 
         <section className="max-md:-order-1 flex-1">
-          <img src='/hero.svg' />
+          <img src='/hero.svg' alt='svg student future schools egypt'/>
         </section>
       </section>
     </header >
