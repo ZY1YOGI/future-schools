58caf73feb5480390c25d19340e2e89d35b08dff
ZY1YOGI <zy1yogi@gmail.com>
2023-06-22 16:48:03 +0300

    v1.1.9 add google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import '@/app/globals.css'
 import Script from 'next/script';
-
+import Head from 'next/head'
 export const metadata: Metadata = {
   applicationName: 'Future Schools',
   authors: [{ name: 'Youssef Amjad', url: '' }],
@@ -11,8 +11,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="light" data-theme="light">
-
+      <Head>
+        {/* <!-- Google Tag Manager --> */}
+        <Script>{`
+          (function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})
+            (window,document,'script','dataLayer','GTM-NV54S9F');
+          `}
+        </Script>
+        {/* <!-- End Google Tag Manager --> */}
+      </Head>
       <body className="antialiased bg-body-light dark:bg-body-dark">
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NV54S9F"
+          height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         {children}
 
 
