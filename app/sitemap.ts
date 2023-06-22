0c9284123a9b83e2364cb917504365a6a0a72b24
ZY1YOGI <zy1yogi@gmail.com>
2023-06-22 17:25:06 +0300

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://future-schools.vercel.app',
+      lastModified: '2023-06-22T14:21:35.961Z',
+    },
+    {
+      url: 'https://future-schools.vercel.app/auth',
+      lastModified: '2023-06-22T14:21:35.961Z',
+    },
+  ]
+}
