d460d377e96d76ff68baa4d3ea38479498556876
ZY1YOGI <zy1yogi@gmail.com>
2023-06-22 16:54:44 +0300

    v1.1.9 add google tag manager V2

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,26 @@ export const metadata: Metadata = {
   applicationName: 'Future Schools',
   authors: [{ name: 'Youssef Amjad', url: '' }],
   creator: 'Youssef Amjad',
+
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png',
+    },
+  ],
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
